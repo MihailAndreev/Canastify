bc302527b6037d3961a6a4a9716295cf878fe5a3
Mihail Andreev <Mihail.andreev@gmail.com>
2026-02-01 13:53:11 +0100

    feat(player): add player module skeleton

---
+++ src/player/playerUI.js
@@ -0,0 +1,13 @@
+/**
+ * Player UI Management
+ * Handles DOM initialization and updates for player interface
+ */
+
+/**
+ * Initialize player UI
+ * Sets up DOM elements and event listeners
+ */
+export function initPlayerUI() {
+  console.log('Initializing player UI...');
+  // TODO: Initialize DOM elements for player UI
+}
